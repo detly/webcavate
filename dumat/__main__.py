edb2d9397e8c8375f75a11f6861c7de9859ff62a
Jason Heeris <jason.heeris@gmail.com>
2015-07-15 20:30:38 +1000

    Changed names of things.

--- dumat/__main__.py
+++ dumat/__main__.py
@@ -1,18 +1,19 @@
 """
 Copyright 2014 Jason Heeris, jason.heeris@gmail.com
 
-This file is part of the dungeon excavator web interface ("Dumat").
+This file is part of the dungeon excavator web interface ("webcavate").
 
-Dumat is free software: you can redistribute it and/or modify it under the terms
-of the GNU General Public License as published by the Free Software Foundation,
-either version 3 of the License, or (at your option) any later version.
+Webcavate is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
 
-Dumat is distributed in the hope that it will be useful, but WITHOUT ANY
+Webcavate is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
 PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with
-Dumat. If not, see <http://www.gnu.org/licenses/>.
+webcavate. If not, see <http://www.gnu.org/licenses/>.
 """
-from dumat.app import main
+from webcavate.app import main
 main()
